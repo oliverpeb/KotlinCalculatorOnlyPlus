f29aff3d65a38ef5db7c69d937dcac0a202cf4d4
oliverpeb <83925702+oliverpeb@users.noreply.github.com>
2023-09-01 13:58:17 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myfirstcalculator/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.myfirstcalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.myfirstcalculator.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding // important
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view: View = binding.root
+        setContentView(view)
+
+    binding.editTextFirstNumber.setOnClickListener {
+
+
+    }
+
+    binding.buttonEquals.setOnClickListener {
+
+        val firstNumber = binding.editTextFirstNumber.text.toString().toDouble()
+        val secondNumber = binding.editTextSecondNumber.text.toString().toDouble()
+
+        val result = firstNumber + secondNumber
+        binding.textView2.text = result.toString()
+
+
+
+
+    }
+
+
+
+
+
+    }
+}
